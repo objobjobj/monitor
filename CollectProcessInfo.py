8163a337402c7d3eff3cb068262bd890b28e015a
YuMoZhiChu <liujiankunmail@gmail.com>
2016-07-20 23:36:04 +0800

    raw info for process info

--- CollectProcessInfo.py
+++ CollectProcessInfo.py
@@ -5,20 +5,20 @@ import json
 class CollectProcessInfo:
 	def _get_process_info(self):
 		result_temp = commands.getoutput("ps -C \"$(xlsclients | cut -d' ' -f3 | paste - -s -d ',')\" --ppid 2 --pid 2 --deselect -o tty,uid,pid,ppid,comm | grep ^\?")
+		return result_temp
 
-		result = result_temp.split('\n')
+		#result = result_temp.split('\n')
 
-		#print result
+		#print result_temp
 
-		res_process = []
+		#res_process = []
 
-		for item in result:
-		  if len(item.split()) < 3 or str.isdigit(item.split()[2]) == False:
-		  	continue
-		  id = int(item.split()[2])
-		  proc = psutil.Process(pid = id)
-		  res_process.append(proc.as_dict(attrs=['pid', 'name', 'username']))
+		#for item in result:
+		#  if len(item.split()) < 3 or str.isdigit(item.split()[2]) == False:
+		#  	continue
+		#  id = int(item.split()[2])
+		#  proc = psutil.Process(pid = id)
+		#  res_process.append(proc.as_dict(attrs=['pid', 'name', 'username']))
 
-		print res_process
-		return json.dumps(res_process)
+		#return json.dumps(res_process)
 
