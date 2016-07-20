f3741d8c11e09c566e5e91d910f3548b18874441
YuMoZhiChu <liujiankunmail@gmail.com>
2016-07-20 23:24:44 +0800

    is digit

--- CollectProcessInfo.py
+++ CollectProcessInfo.py
@@ -4,7 +4,7 @@ import json
 
 class CollectProcessInfo:
 	def _get_process_info(self):
-		result_temp = commands.getoutput("ps -c \"$(xlsclients | cut -d' ' -f3 | paste - -s -d ',')\" --ppid 2 --pid 2 --deselect -o tty,uid,pid,ppid,comm | grep ^\?")
+		result_temp = commands.getoutput("ps -C \"$(xlsclients | cut -d' ' -f3 | paste - -s -d ',')\" --ppid 2 --pid 2 --deselect -o tty,uid,pid,ppid,comm | grep ^\?")
 
 		result = result_temp.split('\n')
 
@@ -13,6 +13,8 @@ class CollectProcessInfo:
 		res_process = []
 
 		for item in result:
+		  if str.isdigit(item.split()[2]) == False:
+		  	continue
 		  id = int(item.split()[2])
 		  proc = psutil.Process(pid = id)
 		  res_process.append(proc.as_dict(attrs=['pid', 'name', 'username']))
