c96372da382f6b902d060ead527e5b239a1a1810
YuMoZhiChu <liujiankunmail@gmail.com>
2016-07-20 23:11:29 +0800

    c and C

--- CollectProcessInfo.py
+++ CollectProcessInfo.py
@@ -4,9 +4,11 @@ import json
 
 class CollectProcessInfo:
 	def _get_process_info(self):
-		result_temp = commands.getoutput("ps -C \"$(xlsclients | cut -d' ' -f3 | paste - -s -d ',')\" --ppid 2 --pid 2 --deselect -o tty,uid,pid,ppid,comm | grep ^\?")
+		result_temp = commands.getoutput("ps -c \"$(xlsclients | cut -d' ' -f3 | paste - -s -d ',')\" --ppid 2 --pid 2 --deselect -o tty,uid,pid,ppid,comm | grep ^\?")
 
 		result = result_temp.split('\n')
+
+		#print result
 
 		res_process = []
 
@@ -14,7 +16,6 @@ class CollectProcessInfo:
 		  id = int(item.split()[2])
 		  proc = psutil.Process(pid = id)
 		  res_process.append(proc.as_dict(attrs=['pid', 'name', 'username']))
-
 
 		return json.dumps(res_process)
 
