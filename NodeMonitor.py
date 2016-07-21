0c56a111e4967a2d92368c6b3ba4983c28fd9d05
YuMoZhiChu <liujiankunmail@gmail.com>
2016-07-21 15:53:57 +0800

    daemon

--- NodeMonitor.py
+++ NodeMonitor.py
@@ -7,6 +7,8 @@ import sys
 import socket
 import fcntl
 import struct
+import os
+import fcntl, sys, time, os
 from kazoo.client import KazooClient
 from kazoo.client import KazooState
 from kazoo.exceptions import ConnectionLossException
@@ -151,6 +153,53 @@ class NodeMonitor:
     		(cmi_just_one_time.collectInfoJustOneTime()).encode(encoding="utf-8"))
         async_obj_just_one_time.rawlink(self._update_info_callback)
 
+def daemonize (stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):  
+     #重定向标准文件描述符（默认情况下定向到/dev/null）  
+    try:   
+        pid = os.fork()   
+          #父进程(会话组头领进程)退出，这意味着一个非会话组头领进程永远不能重新获得控制终端。  
+        if pid > 0:  
+            sys.exit(0)   #父进程退出  
+    except OSError, e:   
+        sys.stderr.write ("fork #1 failed: (%d) %s\n" % (e.errno, e.strerror) )  
+        sys.exit(1)  
+  
+     #从母体环境脱离  
+    os.chdir("/")  #chdir确认进程不保持任何目录于使用状态，否则不能umount一个文件系统。也可以改变到对于守护程序运行重要的文件所在目录  
+    os.umask(0)    #调用umask(0)以便拥有对于写的任何东西的完全控制，因为有时不知道继承了什么样的umask。  
+    os.setsid()    #setsid调用成功后，进程成为新的会话组长和新的进程组长，并与原来的登录会话和进程组脱离。  
+  
+     #执行第二次fork  
+    try:   
+        pid = os.fork()   
+        if pid > 0:  
+            sys.exit(0)   #第二个父进程退出  
+    except OSError, e:   
+        sys.stderr.write ("fork #2 failed: (%d) %s\n" % (e.errno, e.strerror) )  
+        sys.exit(1)  
+  
+     #进程已经是守护进程了，重定向标准文件描述符  
+  
+    for f in sys.stdout, sys.stderr: f.flush()  
+    si = open(stdin, 'r')  
+    so = open(stdout, 'a+')  
+    se = open(stderr, 'a+', 0)  
+    os.dup2(si.fileno(), sys.stdin.fileno())    #dup2函数原子化关闭和复制文件描述符  
+    os.dup2(so.fileno(), sys.stdout.fileno())  
+    os.dup2(se.fileno(), sys.stderr.fileno())  
+  
+# running main  
+def main(argv1):    
+    sys.stdout.write('Node Monitor daemon started with pid %d\n' % os.getpid())  
+    sys.stdout.write('Node Monitor daemon stdout output\n')  
+    sys.stderr.write('Node Monitor daemon stderr output\n')  
+
+    nm = NodeMonitor(argv1)
+    nm.start_zk()
+    nm._update_info_just_one_time()
+    nm.start_update_info()
+
+
 if __name__ == "__main__":
     ## set is server or not
     argv1 = ''
@@ -158,10 +207,7 @@ if __name__ == "__main__":
         if sys.argv[1] == '-server':
             argv1 = '-server'
 
-
-    nm = NodeMonitor(argv1)
-    nm.start_zk()
-    nm._update_info_just_one_time()
-    nm.start_update_info()
+    daemonize('/dev/null','/tmp/daemon_stdout.log','/tmp/daemon_error.log')  
+    main(argv1)
     
         
